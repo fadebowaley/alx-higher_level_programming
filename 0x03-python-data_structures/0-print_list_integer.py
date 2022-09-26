5ecc341d9cea25df2882ebc78e6186aa132a3b04
Adem0ola Adebowale <fadebowaley@gmail.com>
2022-09-26 01:49:12 -0700

    update for python excercise

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    """ Printing a list of Integers """
-    for i in my_list:
-        print(i):wq
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
